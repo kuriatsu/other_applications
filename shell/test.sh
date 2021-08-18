d4f645bdc4b53d6913b969714310ba8db58f033d
kuriatsu <kuriatsubayashi712@gmail.com>
2021-08-18 23:42:59 +0900

    refresh backup scrips

--- shell/test.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-source devel/setup.bash
-roslaunch carla_helper carla_helper.launch &
-roslaunch teleop_carla teleop_carla.launch &
-roslaunch ras_carla ras_carla.launch &
-python src/carla_helper/script/camera.py --rolename ros_camera_back --res 800x600
