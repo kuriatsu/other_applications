1409985cf721bb0d065a2e00cc237fe4cfe8eadf
kuriatsu <kuriatsubayashi712@gmail.com>
2021-11-06 18:12:30 +0900

    add

--- python/carla_experiment/analyze.py
+++ python/carla_experiment/analyze.py
@@ -111,20 +111,25 @@ summary_df = pd.read_csv('/media/kuriatsu/SamsungKURI/master_study_bag/202102exp
 nasa_df = pd.read_csv('/media/kuriatsu/SamsungKURI/master_study_bag/202102experiment/result/nasa-tlx.csv')
 rank_df = pd.read_csv('/media/kuriatsu/SamsungKURI/master_study_bag/202102experiment/result/rank.csv')
 subjects = summary_df.subject.drop_duplicates()
-experiments = summary_df.experiment_type.drop_duplicates()
+experiments = ['baseline', 'control', 'button', 'touch']
 
 ################################################################
-################################################################
-
 print('counter variance')
+################################################################
 summary_df["cv"] = summary_df.std_vel / summary_df.mean_vel
 
 _, norm_p = stats.shapiro(summary_df.cv.dropna())
-_, var_p = stats.levene(summary_df[summary_df.experiment_type == 'baseline'].cv.dropna(), summary_df[summary_df.experiment_type == 'control'].cv.dropna(), summary_df[summary_df.experiment_type == 'button'].cv.dropna(), summary_df[summary_df.experiment_type == 'touch'].cv.dropna(), center='median')
-if norm_p < 0.05 or var_p < 0.05:
-    print('steel-dwass', sp.posthoc_dscf(summary_df, val_col='cv', group_col='experiment_type'))
-else:
+_, var_p = stats.levene(
+    summary_df[summary_df.experiment_type == 'baseline'].cv.dropna(),
+    summary_df[summary_df.experiment_type == 'control'].cv.dropna(),
+    summary_df[summary_df.experiment_type == 'button'].cv.dropna(),
+    summary_df[summary_df.experiment_type == 'touch'].cv.dropna(),
+    center='median'
+    )
 
+if norm_p < 0.05 or var_p < 0.05:
+    print('steel-dwass\n', sp.posthoc_dscf(summary_df, val_col='cv', group_col='experiment_type'))
+else:
     multicomp_result = multicomp.MultiComparison(np.array(summary_df.dropna(how='any').cv, dtype="float64"), summary_df.dropna(how='any').experiment_type)
     print('levene', multicomp_result.tukeyhsd().summary())
 
@@ -135,60 +140,154 @@ plt.show()
 ################################################################
 print('intervene accuracy')
 ################################################################
-accuracy = pd.DataFrame(columns=['experiment', 'result'])
+inttype_accuracy = pd.DataFrame(columns=['experiment', 'result'])
 for index, row in summary_df.iterrows():
     if row.actor_action == 'cross':
         buf = pd.DataFrame([(row.experiment_type, np.isnan(row.intervene_vel)) ], columns=['experiment', 'result'])
-        accuracy = accuracy.append(buf, ignore_index=True)
+        inttype_accuracy = inttype_accuracy.append(buf, ignore_index=True)
     elif row.actor_action == 'pose':
         if np.isnan(row.intervene_vel):
             buf = pd.DataFrame([(row.experiment_type, False)], columns=['experiment', 'result'])
-            accuracy = accuracy.append(buf, ignore_index=True)
+            inttype_accuracy = inttype_accuracy.append(buf, ignore_index=True)
         else:
             buf = pd.DataFrame([(row.experiment_type, (row.intervene_vel > 1.0))], columns=['experiment', 'result'])
-            accuracy = accuracy.append(buf, ignore_index=True)
+            inttype_accuracy = inttype_accuracy.append(buf, ignore_index=True)
 
-accuracy_cross = pd.crosstab(accuracy.experiment, accuracy.result)
-stats.chi2_contingency(accuracy_cross)
+inttype_accuracy_cross = pd.crosstab(inttype_accuracy.experiment, inttype_accuracy.result)
+stats.chi2_contingency(inttype_accuracy_cross)
 
-_, p = stats.levene(summary_df.baseline, summary_df.control, summary_df.button, summary_df.touch, center='median')
-print('levene-first last_intervene_time', p)
-if p > 0.05:
-    melted_df = pd.melt(intervene_acc, var_name='experiment_type', value_name='intervene_vel')
-    multicomp_result = multicomp.MultiComparison(np.array(melted_df['intervene_vel'], dtype="float64"), melted_df['experiment_type'])
-    print(multicomp_result.tukeyhsd().summary())
+subject_accuracy = pd.DataFrame(columns=['subject', 'result'])
+for index, row in summary_df.iterrows():
+    if row.actor_action == 'cross':
+        buf = pd.DataFrame([(row.subject, np.isnan(row.intervene_vel)) ], columns=['subject', 'result'])
+        subject_accuracy = subject_accuracy.append(buf, ignore_index=True)
+    elif row.actor_action == 'pose':
+        if np.isnan(row.intervene_vel):
+            buf = pd.DataFrame([(row.subject, False)], columns=['subject', 'result'])
+            subject_accuracy = subject_accuracy.append(buf, ignore_index=True)
+        else:
+            buf = pd.DataFrame([(row.subject, (row.intervene_vel > 1.0))], columns=['subject', 'result'])
+            subject_accuracy = subject_accuracy.append(buf, ignore_index=True)
 
+subject_accuracy_cross = pd.crosstab(subject_accuracy.subject, subject_accuracy.result)
+stats.chi2_contingency(subject_accuracy_cross)
+
+
+################################################################
+print('intervene time')
+################################################################
+pose_df = summary_df[summary_df.actor_action == 'pose']
+_, norm_p = stats.shapiro(pose_df.first_intervene_time.dropna())
+_, var_p = stats.levene(pose_df[pose_df.experiment_type == 'baseline'].first_intervene_time.dropna(), pose_df[pose_df.experiment_type == 'control'].first_intervene_time.dropna(), pose_df[pose_df.experiment_type == 'button'].first_intervene_time.dropna(), pose_df[pose_df.experiment_type == 'touch'].first_intervene_time.dropna(), center='median')
+if norm_p < 0.05 or var_p < 0.05:
+    print('steel-dwass\n', sp.posthoc_dscf(pose_df, val_col='first_intervene_time', group_col='experiment_type'))
+else:
+    multicomp_result = multicomp.MultiComparison(np.array(pose_df.dropna(how='any').first_intervene_time, dtype="float64"), pose_df.dropna(how='any').experiment_type)
+    print('levene', multicomp_result.tukeyhsd().summary())
+
+
+pose_df = summary_df[summary_df.actor_action == 'pose']
+_, norm_p = stats.shapiro(pose_df.first_intervene_time.dropna())
+_, var_p = stats.levene(
+    pose_df[pose_df.subject == 'ando'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'aso'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'hikosaka'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'ichiki'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'ienaga'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'ikai'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'isobe'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'ito'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'kato'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'matsubara'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'nakakuki'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'nakatani'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'negi'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'otake'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'sumiya'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'taga'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'yamamoto'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'yasuhara'].first_intervene_time.dropna(),
+    center='median'
+    )
+
+_, anova_p = stats.kruskal(
+    pose_df[pose_df.subject == 'ando'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'aso'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'hikosaka'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'ichiki'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'ienaga'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'ikai'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'isobe'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'ito'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'kato'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'matsubara'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'nakakuki'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'nakatani'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'negi'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'otake'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'sumiya'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'taga'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'yamamoto'].first_intervene_time.dropna(),
+    pose_df[pose_df.subject == 'yasuhara'].first_intervene_time.dropna(),
+)
+
+if anova_p < 0.05:
+    if norm_p < 0.05 or var_p < 0.05:
+        print('steel-dwass\n', sp.posthoc_dscf(pose_df, val_col='first_intervene_time', group_col='subject'))
+    else:
+        multicomp_result = multicomp.MultiComparison(np.array(pose_df.dropna(how='any').first_intervene_time, dtype="float64"), pose_df.dropna(how='any').subject)
+        print('levene', multicomp_result.tukeyhsd().summary())
+
+################################################################
 print("avoid decel rate")
-intervene_speed = pd.DataFrame(index=subjects, columns=experiments)
-for subject in subjects:
-    pose_df = summary_df[(summary_df.subject == subject) & (summary_df.actor_action == "pose")]
-    for experiment in experiments:
-        buf = pose_df[pose_df.experiment_type==experiment].intervene_vel > 40.0
-        rate = buf.sum() / len(pose_df[pose_df.experiment_type==experiment])
-        intervene_speed.at[subject, experiment] = rate
+################################################################
+avoid_decel_df = pd.DataFrame(columns=['experiment', 'result'])
+for index, row in summary_df[summary_df.actor_action == 'pose'].iterrows():
+    if row.actor_action == 'pose':
+        if np.isnan(row.intervene_vel):
+            buf = pd.DataFrame([(row.experiment_type, False)], columns=['experiment', 'result'])
+            avoid_decel_df = avoid_decel_df.append(buf, ignore_index=True)
+        else:
+            buf = pd.DataFrame([(row.experiment_type, (row.intervene_vel > 40.0))], columns=['experiment', 'result'])
+            avoid_decel_df = avoid_decel_df.append(buf, ignore_index=True)
 
-_, p = stats.levene(intervene_speed.baseline, intervene_speed.control, intervene_speed.button, intervene_speed.touch, center='median')
-print('levene-first last_intervene_time', p)
-axes = sns.boxplot(data=intervene_speed, showmeans=True, meanline=True, meanprops={"linestyle":"--", "color":"Red"})
-plt.show()
+avoid_decel_cross = pd.crosstab(avoid_decel_df.experiment, avoid_decel_df.result)
+stats.chi2_contingency(avoid_decel_cross)
 
-melted_df = pd.melt(intervene_speed, var_name='experiment_type', value_name='intervene_vel')
-multicomp_result = multicomp.MultiComparison(np.array(melted_df['intervene_vel'], dtype="float64"), melted_df['experiment_type'])
-print(multicomp_result.tukeyhsd().summary())
-
-print("accuracy vs avoid decel")
+################################################################
+print("accuracy vs intervene_time")
+################################################################
+accuracy_list = [
+    inttype_accuracy_cross.at['baseline', True] / (inttype_accuracy_cross.at['baseline', True] + inttype_accuracy_cross.at['baseline', False]),
+    inttype_accuracy_cross.at['control', True] / (inttype_accuracy_cross.at['control', True] + inttype_accuracy_cross.at['control', False]),
+    inttype_accuracy_cross.at['button', True] / (inttype_accuracy_cross.at['button', True] + inttype_accuracy_cross.at['button', False]),
+    inttype_accuracy_cross.at['touch', True] / (inttype_accuracy_cross.at['touch', True] + inttype_accuracy_cross.at['touch', False])
+]
+intervene_time_mean_list = [
+    summary_df[(summary_df.actor_action == 'pose') & (summary_df.experiment_type == 'baseline')].first_intervene_time.mean(),
+    summary_df[(summary_df.actor_action == 'pose') & (summary_df.experiment_type == 'control')].first_intervene_time.mean(),
+    summary_df[(summary_df.actor_action == 'pose') & (summary_df.experiment_type == 'button')].first_intervene_time.mean(),
+    summary_df[(summary_df.actor_action == 'pose') & (summary_df.experiment_type == 'touch')].first_intervene_time.mean(),
+]
+intervene_time_sem_list = [
+    summary_df[(summary_df.actor_action == 'pose') & (summary_df.experiment_type == 'baseline')].first_intervene_time.sem(),
+    summary_df[(summary_df.actor_action == 'pose') & (summary_df.experiment_type == 'control')].first_intervene_time.sem(),
+    summary_df[(summary_df.actor_action == 'pose') & (summary_df.experiment_type == 'button')].first_intervene_time.sem(),
+    summary_df[(summary_df.actor_action == 'pose') & (summary_df.experiment_type == 'touch')].first_intervene_time.sem(),
+]
 _, axes = plt.subplots()
-for experiment in experiments:
-    axes.errorbar(intervene_acc.mean()[experiment], intervene_speed.mean()[experiment], xerr=intervene_acc.sem()[experiment], yerr=intervene_speed.sem()[experiment], marker='o', capsize=5, label=experiment)
+for i, experiment in enumerate(experiments):
+    axes.errorbar(accuracy_list[i], intervene_time_mean_list[i], yerr=intervene_time_sem_list[i], marker='o', capsize=5, label=experiment)
 
 axes.set_xlim(0, 1.0)
-axes.set_ylim(0, 1.0)
-axes.set_xlabel('Unnecessary intervention rate', fontsize=15)
-axes.set_ylabel('Avoid deceleration rate', fontsize=15)
-axes.legend(loc='upper right', fontsize=15)
+# axes.set_ylim(0, 1.0)
+axes.set_xlabel('Intervention accuracy', fontsize=15)
+axes.set_ylabel('Intervention time [s]', fontsize=15)
+axes.legend(loc='lower left', fontsize=15)
 axes.tick_params(axis='x', labelsize=12)
 axes.tick_params(axis='y', labelsize=12)
 plt.show()
+
 ################################################################
 print("intervene vel range stacked bar plot")
 ################################################################
@@ -255,43 +354,30 @@ if p > 0.05:
 print('min_vel rerative to baseline plot')
 ################################################################
 
-intervene_speed_rate = pd.DataFrame(columns=['experiment_type', 'range', 'rate'], index = [])
-# intervene_speed_rate = pd.DataFrame(columns=['subjects', 'experiment_type', 'range', 'rate'], index = [])
-
-# for subject in subjects:
-for experiment in experiments:
-    for thres in [-1.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0]:
-        rate = len(summary_df[(summary_df.experiment_type == experiment) & (summary_df.actor_action == 'pose') & (summary_df.min_vel > 45.0)].min_vel) / len(summary_df[(summary_df.experiment_type == experiment) & (summary_df.actor_action == 'pose')].min_vel)
-        buf_df = pd.Series([experiment, thres, rate], index=intervene_speed_rate.columns)
-        intervene_speed_rate = intervene_speed_rate.append(buf_df, ignore_index=True)
-
-intervene_speed_rate_summary = pd.DataFrame(columns=['experiment_type', 'range', 'mean', 'std_err'], index = [])
-for experiment in experiments:
-    for thres in [0.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0]:
-        mean = intervene_speed_rate[(intervene_speed_rate.experiment_type == experiment) & (intervene_speed_rate.range == thres)].rate.mean()
-        std_err = intervene_speed_rate[(intervene_speed_rate.experiment_type == experiment) & (intervene_speed_rate.range == thres)].rate.sem()
-        buf_df = pd.Series([experiment, thres, mean, std_err], index=intervene_speed_rate_summary.columns)
-        intervene_speed_rate_summary = intervene_speed_rate_summary.append(buf_df, ignore_index=True)
-
-for i, range in enumerate(intervene_speed_rate.range):
-    if range == -1.0:
-        intervene_speed_rate.at[i, "range"] = 0.0
-intervene_speed_rate.at[intervene_speed_rate.range == -1.0].range = 0.0
-axes = sns.pointplot(x='range', y='rate', data=intervene_speed_rate, hue='experiment_type')
-
+# intervene_speed_rate = pd.DataFrame(columns=['experiment_type', 'range', 'rate'], index = [])
+# # intervene_speed_rate = pd.DataFrame(columns=['subjects', 'experiment_type', 'range', 'rate'], index = [])
+#
+# # for subject in subjects:
 # for experiment in experiments:
-#     for thres in [10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0]:
-#         row = intervene_speed_rate_summary[(intervene_speed_rate_summary.range == thres) & (intervene_speed_rate_summary.experiment_type == experiment)]
-#         axes.errorbar(thres, row['mean'], yerr=row['std_err'], marker='o', capsize=5, label=experiment)
-
-axes.set_xlim([0, 50])
-axes.set_ylim(0, 1)
-axes.set_xlabel('Accuracy', fontsize=15)
-axes.set_ylabel('Intervene Time [s]', fontsize=15)
-# axes.legend(loc='lower left', fontsize=15)
-axes.tick_params(axis='x', labelsize=12)
-axes.tick_params(axis='y', labelsize=12)
-plt.show()
+#     for thres in [-1.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0]:
+#         rate = len(summary_df[(summary_df.experiment_type == experiment) & (summary_df.actor_action == 'pose') & (summary_df.min_vel > 45.0)].min_vel) / len(summary_df[(summary_df.experiment_type == experiment) & (summary_df.actor_action == 'pose')].min_vel)
+#         buf_df = pd.Series([experiment, thres, rate], index=intervene_speed_rate.columns)
+#         intervene_speed_rate = intervene_speed_rate.append(buf_df, ignore_index=True)
+#
+# intervene_speed_rate_summary = pd.DataFrame(columns=['experiment_type', 'range', 'mean', 'std_err'], index = [])
+# for experiment in experiments:
+#     for thres in [0.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0]:
+#         mean = intervene_speed_rate[(intervene_speed_rate.experiment_type == experiment) & (intervene_speed_rate.range == thres)].rate.mean()
+#         std_err = intervene_speed_rate[(intervene_speed_rate.experiment_type == experiment) & (intervene_speed_rate.range == thres)].rate.sem()
+#         buf_df = pd.Series([experiment, thres, mean, std_err], index=intervene_speed_rate_summary.columns)
+#         intervene_speed_rate_summary = intervene_speed_rate_summary.append(buf_df, ignore_index=True)
+#
+# for i, range in enumerate(intervene_speed_rate.range):
+#     if range == -1.0:
+#         intervene_speed_rate.at[i, "range"] = 0.0
+# intervene_speed_rate.at[intervene_speed_rate.range == -1.0].range = 0.0
+# axes = sns.pointplot(x='range', y='rate', data=intervene_speed_rate, hue='experiment_type')
+#
 
 ################################################################
 print('stop rate')
